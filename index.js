fac34d514fd27d1db57da0045e3a278a9cf9a30f
Joseph Plair <jpleeze@yahoo.com>
2023-04-15 21:31:38 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let response;
+ if (distance < 400){  
+ response = 'This one is on me!';
+ return response;
+ }
+ else if (distance >= 400 && distance <= 2000){
+  response = 'That will be twenty bucks.';
+  return response;
+ }
+ else if (distance > 2000 && distance <= 2500){
+  response = 'I will gladly take your thirty bucks.';
+  return response;
+ }
+ else if (distance > 2500){
+  response = 'No can do.';
+  return response;
+ }
 }
+function ternaryCheckCity(city){
+ return city == 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  }
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+  case 'generous':
+    return ('Thank you so much.');
+  case 'not as generous':
+    return ('Thank you.');
+  case 'thanks for everything':
+    return ('Bye.');
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  }
+  
+  
+  
 }
